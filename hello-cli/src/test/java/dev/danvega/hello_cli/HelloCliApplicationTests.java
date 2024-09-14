ccd09e123ba46d13b2927a6dee54e2450ace60f0
Eduardo Orlandi Melle <eduardomelle@gmail.com>
2024-09-14 12:33:42 -0300

    first commit

---
+++ hello-cli/src/test/java/dev/danvega/hello_cli/HelloCliApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danvega.hello_cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
